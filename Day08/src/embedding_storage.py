5fe68ebc5b5bbe51ffc700068b9a1ccbd83e8c94
Stefano Mavilio <st.mavilio@gmail.com>
2024-12-11 19:57:03 +0100

    feat(pdf-rag): get text and embedding based on index

--- Day08/src/embedding_storage.py
+++ Day08/src/embedding_storage.py
@@ -5,19 +5,25 @@ import os
 class EmbeddingStorage:
     def __init__(self, file_path):
         self.file_path = file_path
-        self.embeddings = []
+        self.data = []
 
         if os.path.exists(self.file_path):
             self.load_embeddings()
 
-    def save_embeddings(self, embeddings):
-        self.embeddings = embeddings
+    def save_embeddings(self, embeddings, texts):
+        self.data = [{"embedding": embedding, "text": text} for embedding, text in zip(embeddings, texts)]
         with open(self.file_path, 'w') as f:
-            json.dump(self.embeddings, f)
+            json.dump(self.data, f)
 
     def load_embeddings(self):
         with open(self.file_path, 'r') as f:
-            self.embeddings = json.load(f)
+            self.data = json.load(f)
 
-    def get_embeddings(self):
-        return self.embeddings
+    def get_embedding(self, index):
+        return self.data[index]['embedding'] if 0 <= index < len(self.data) else None
+
+    def get_text(self, index):
+        return self.data[index]['text'] if 0 <= index < len(self.data) else None
+
+    def get_all_embeddings(self):
+        return [item['embedding'] for item in self.data]
